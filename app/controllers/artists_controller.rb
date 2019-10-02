d535dc12292d9c049ae0fa53878792ec42e23a6d
Joy Lin <vvlnote@hotmail.com>
2019-10-02 04:36:12 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,14 @@
 class ArtistsController < ApplicationController
+
+    get '/artists' do
+    @artists = Artist.all
+    erb :'/artists/index'
+  end
+
+   get '/artists/:slug' do
+    @artist = Artist.find {|artist| artist.slug == params[:slug]}
+    @songs = @artists.songs
+    @genres = @artists.genres
+    erb :'/artists/show'
+  end
 end
---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,8 @@
+
+module Slugifiable
+module InstanceMethods
+  def slug
+    self.name.downcase.gsub(" ", "-")
+  end
+end
+end
